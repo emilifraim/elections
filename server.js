a35e087e7f5266223a72e5ca28c616484948d4fa
Emil Ifraimov <emilifraim@gmail.com>
2019-03-14 14:38:17 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(bodyParser.json());
 // USE Routes
 app.use('/api/elections', elections);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const port = process.env.PORT || 5040;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
